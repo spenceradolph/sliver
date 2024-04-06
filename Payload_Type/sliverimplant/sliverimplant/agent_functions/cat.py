1e6c146cfa26a95053caa371e4bec3e2e0b4d327
spenceradolph <spenceradolph@ymail.com>
2024-04-06 18:51:18 +0000

    cat command

---
+++ Payload_Type/sliverimplant/sliverimplant/agent_functions/cat.py
@@ -0,0 +1,57 @@
+# TODO: learn how to python, this is probably not right
+from ..SliverRequests import SliverAPI
+
+
+from mythic_container.MythicCommandBase import *
+from mythic_container.MythicRPC import *
+from mythic_container.PayloadBuilder import *
+
+class CatArguments(TaskArguments):
+    def __init__(self, command_line, **kwargs):
+        super().__init__(command_line, **kwargs)
+        self.args = [
+            # TODO: add arguments here for mtls
+            CommandParameter(
+                name="path",
+                description="path to file",
+                type=ParameterType.String
+            ),
+        ]
+
+    async def parse_arguments(self):
+        self.load_args_from_json_string(self.command_line)
+
+
+class Cat(CommandBase):
+    cmd = "cat"
+    needs_admin = False
+    help_cmd = "cat"
+    description = "cat a file"
+    version = 1
+    author = "Spencer Adolph"
+    argument_class = CatArguments
+    attackmapping = []
+
+    async def create_go_tasking(self, taskData: MythicCommandBase.PTTaskMessageAllData) -> MythicCommandBase.PTTaskCreateTaskingMessageResponse:
+        # just download and don't create a file, show the output to user
+        # sliver py doesn't have a direct 'cat' method to use
+        plaintext = await SliverAPI.download(taskData)
+
+        # TODO: other error handling
+
+        await SendMythicRPCResponseCreate(MythicRPCResponseCreateMessage(
+            TaskID=taskData.Task.ID,
+            Response=f"{plaintext.decode('utf-8')}".encode("UTF8"),
+        ))
+
+        # TODO: error handling
+        response = MythicCommandBase.PTTaskCreateTaskingMessageResponse(
+            TaskID=taskData.Task.ID,
+            Success=True,
+            Completed=True
+        )
+        return response
+
+    async def process_response(self, task: PTTaskMessageAllData, response: any) -> PTTaskProcessResponseMessageResponse:
+        resp = PTTaskProcessResponseMessageResponse(TaskID=task.Task.ID, Success=True)
+        return resp
