4cbf6b9450099d75b37fc1cc2f1213c5083ec52f
spenceradolph <spenceradolph@ymail.com>
2024-04-06 16:18:39 +0000

    download is working

---
+++ Payload_Type/sliverimplant/sliverimplant/agent_functions/download.py
@@ -0,0 +1,69 @@
+# TODO: learn how to python, this is probably not right
+from ..SliverRequests import SliverAPI
+import gzip
+
+from mythic_container.MythicCommandBase import *
+from mythic_container.MythicRPC import *
+from mythic_container.PayloadBuilder import *
+
+class DownloadArguments(TaskArguments):
+    def __init__(self, command_line, **kwargs):
+        super().__init__(command_line, **kwargs)
+        self.args = [
+            # TODO: add arguments here for mtls
+            CommandParameter(
+                name="path",
+                description="path to file",
+                type=ParameterType.String
+            ),
+        ]
+
+    async def parse_arguments(self):
+        self.load_args_from_json_string(self.command_line)
+
+
+class Download(CommandBase):
+    cmd = "download"
+    needs_admin = False
+    help_cmd = "download"
+    description = "Download a file"
+    version = 1
+    author = "Spencer Adolph"
+    argument_class = DownloadArguments
+    attackmapping = []
+
+    async def create_go_tasking(self, taskData: MythicCommandBase.PTTaskMessageAllData) -> MythicCommandBase.PTTaskCreateTaskingMessageResponse:
+        client = await SliverAPI.create_sliver_client(taskData)
+        
+        interact = await client.interact_beacon(taskData.Payload.UUID)
+        download_task = await interact.download(remote_path=taskData.args.get_arg('path'))
+        download = await download_task
+
+        plaintext = gzip.decompress(download.Data)
+
+        results = await SendMythicRPCFileCreate(MythicRPCFileCreateMessage(
+            TaskID=taskData.Task.ID,
+            RemotePathOnTarget=taskData.args.get_arg('path'),
+            FileContents=plaintext,
+            IsScreenshot=False,
+            IsDownloadFromAgent=True
+        ))
+
+        # TODO: other error handling
+
+        await SendMythicRPCResponseCreate(MythicRPCResponseCreateMessage(
+            TaskID=taskData.Task.ID,
+            Response=f"agent file id == {results.AgentFileId}".encode("UTF8"),
+        ))
+
+        # TODO: error handling
+        response = MythicCommandBase.PTTaskCreateTaskingMessageResponse(
+            TaskID=taskData.Task.ID,
+            Success=True,
+            Completed=True
+        )
+        return response
+
+    async def process_response(self, task: PTTaskMessageAllData, response: any) -> PTTaskProcessResponseMessageResponse:
+        resp = PTTaskProcessResponseMessageResponse(TaskID=task.Task.ID, Success=True)
+        return resp
