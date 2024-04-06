9c7c087f8b315f7226a67d77867ed15a2e545cfb
spenceradolph <spenceradolph@ymail.com>
2024-04-06 16:54:06 +0000

    upload functionality proof of concept

---
+++ Payload_Type/sliverimplant/sliverimplant/agent_functions/upload.py
@@ -0,0 +1,68 @@
+# TODO: learn how to python, this is probably not right
+from ..SliverRequests import SliverAPI
+# import gzip
+
+from mythic_container.MythicCommandBase import *
+from mythic_container.MythicRPC import *
+from mythic_container.PayloadBuilder import *
+
+class UploadArguments(TaskArguments):
+    def __init__(self, command_line, **kwargs):
+        super().__init__(command_line, **kwargs)
+        self.args = [
+            # TODO: add arguments here for mtls
+            CommandParameter(
+                name="path",
+                description="full path to create file",
+                type=ParameterType.String
+            ),
+            CommandParameter(
+                name="uuid",
+                description="uuid of existing file to upload",
+                type=ParameterType.String
+            ),
+        ]
+
+    async def parse_arguments(self):
+        self.load_args_from_json_string(self.command_line)
+
+
+class Upload(CommandBase):
+    cmd = "upload"
+    needs_admin = False
+    help_cmd = "upload"
+    description = "Upload a file"
+    version = 1
+    author = "Spencer Adolph"
+    argument_class = UploadArguments
+    attackmapping = []
+
+    async def create_go_tasking(self, taskData: MythicCommandBase.PTTaskMessageAllData) -> MythicCommandBase.PTTaskCreateTaskingMessageResponse:
+        filestuff = await SendMythicRPCFileGetContent(MythicRPCFileGetContentMessage(
+            AgentFileId=taskData.args.get_arg('uuid')
+        ))
+        client = await SliverAPI.create_sliver_client(taskData)
+        interact = await client.interact_beacon(taskData.Payload.UUID)
+        upload_task = await interact.upload(
+            remote_path=taskData.args.get_arg('path'),
+            data=filestuff.Content
+        )
+        await upload_task
+
+        # TODO: other error handling
+
+        await SendMythicRPCResponseCreate(MythicRPCResponseCreateMessage(
+            TaskID=taskData.Task.ID,
+            Response="upload success?".encode("UTF8"),
+        ))
+
+        response = MythicCommandBase.PTTaskCreateTaskingMessageResponse(
+            TaskID=taskData.Task.ID,
+            Success=True,
+            Completed=True
+        )
+        return response
+
+    async def process_response(self, task: PTTaskMessageAllData, response: any) -> PTTaskProcessResponseMessageResponse:
+        resp = PTTaskProcessResponseMessageResponse(TaskID=task.Task.ID, Success=True)
+        return resp
